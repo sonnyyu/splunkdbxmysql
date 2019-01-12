472fe164a3accff8e7f559fd9911e8a50dbcdc79
sonnyyu <sonnyyuirm@gmail.com>
2019-01-12 02:35:02 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -38,7 +38,8 @@ fi
 if [[ "$INSTALL_DBX" == "1" ]]; then
 cd /opt/splunk/bin
 wget https://www.dropbox.com/s/djjn9to4b4r3fy6/splunk-db-connect_314.tgz
-./splunk install app splunk-db-connect_314.tgz
+./splunk install app splunk-db-connect_314.tgz -auth 'admin:${PLUNK_PASSWORD}'
+
 ./splunk restart
 cd /opt/splunk/etc/apps/splunk_app_db_connect/drivers/
 wget https://www.dropbox.com/s/gpardxaqelw136t/mysql-connector-java-8.0.13.jar
