1c34f928aa89b0c0af962d10818b86b25235a4fc
sonnyyu <sonnyyuirm@gmail.com>
2019-01-12 01:15:03 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -45,6 +45,7 @@ _db_sample_file="world.sql"
 cd ${DIR}
 
 wget https://www.dropbox.com/s/j39s3baatjgf6c8/world.sql
+
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y mysql-server || exit 1
 sudo mysqladmin -u root password ${_db_password} || exit 1
 sudo mysql -u${_db_user} -p  -e "ALTER USER '${_db_user}'@'localhost' IDENTIFIED WITH mysql_native_password BY '${_db_password}';"
